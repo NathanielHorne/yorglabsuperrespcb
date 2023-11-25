2ecef28eadecdea75446dfff19c56af2b9edfae9
Horne467 (Mint Laptop) <horne467@byu.edu>
2023-11-25 14:48:26 -0700

    Homing behavior achieved.

--- stepper_stage_PCB/super_res_code/super_res_code.ino
+++ stepper_stage_PCB/super_res_code/super_res_code.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #define stepPin1  10
 #define dirPin1   11
 #define enPin1    12
@@ -34,17 +36,24 @@ int ena1 = LOW;
 int direction_bool1 = LOW;
 unsigned long step_1_time_index = micros();
 unsigned int step_1_time_tolerance = 0;
-long num_steps1 = 0;
-long placeholder = 0;
-int off_track1 = LOW;
+uint64_t num_steps1 = 0;
+int is_off_track1 = LOW;
+int is_at_middle1 = LOW;
 
 unsigned int joy_input2 = 0;
 int ena2 = LOW;
 int direction_bool2 = HIGH;
 unsigned long step_2_time_index = micros();
 unsigned int step_2_time_tolerance = 0;
-long num_steps2 = 0;
-int off_track2 = LOW;
+uint64_t num_steps2 = 0;
+int is_off_track2 = LOW;
+int is_at_middle2 = LOW;
+
+void get_joystick_inputs();
+
+void get_is_off_tracks();
+
+void get_back_on_track();
 
 void set_direction_bools();
 
@@ -60,9 +69,11 @@ unsigned int stepper_wait_time(const int& input);
 
 void planning();
 
-void move_stepper(const unsigned int& step_pin, unsigned long& time_index, const unsigned int& time_tolerance, const unsigned int& direction_bool, long& num_steps, const int& ena);
+void move_stepper(const unsigned int& step_pin, unsigned long& time_index, const unsigned int& time_tolerance, const unsigned int& direction_bool, uint64_t& num_steps, const int& ena);
 
 void action();
+
+void print(uint64_t value);
 
 void setup() {
   Serial.begin(9600);
@@ -85,7 +96,7 @@ void setup() {
 
   set_direction_bools();
 
-//  startup();
+  startup();
 }
 
 void loop() {
@@ -95,6 +106,40 @@ void loop() {
   planning();
 
   action();
+}
+
+void get_joystick_inputs() {
+  joy_input1 = analogRead(joy_in1);
+  joy_input2 = analogRead(joy_in2);
+}
+
+void get_is_off_tracks() {
+  is_off_track1 = digitalRead(gate_switch1);
+  is_off_track2 = digitalRead(gate_switch2);
+}
+
+void get_back_on_track() {
+  if(is_off_track1){
+    direction_bool1 = !direction_bool1;
+  }
+
+  if(is_off_track2) {
+    direction_bool2 = !direction_bool2;
+  }
+
+  set_direction_bools();
+
+  do {
+    get_is_off_tracks();
+    
+    if(is_off_track1) {
+      move_stepper(stepPin1, step_1_time_index, startup_speed*4, direction_bool1, num_steps1, ena1);
+    }
+    
+    if(is_off_track2) {
+      move_stepper(stepPin2, step_2_time_index, startup_speed*4, direction_bool2, num_steps2, ena2);
+    }
+  } while(is_off_track1 || is_off_track2);
 }
 
 void set_en_pins() {
@@ -108,52 +153,46 @@ void set_direction_bools() {
 }
 
 void startup() {
-  while(digitalRead(gate_switch1) == 0) {
-    move_stepper(stepPin1, step_1_time_index, startup_speed, direction_bool1, num_steps1, ena1);
-  }
-  direction_bool1 = HIGH;
-  set_direction_bools();
-  while(digitalRead(gate_switch1) == 1) {
-    move_stepper(stepPin1, step_1_time_index, startup_speed, direction_bool1, num_steps1, ena1);
-  }
-  num_steps1 = 0;
+  do {
+    get_is_off_tracks();
+    
+    if(!(is_off_track1)) {
+      move_stepper(stepPin1, step_1_time_index, startup_speed, direction_bool1, num_steps1, ena1);
+    }
+    
+    if(!(is_off_track2)) {
+      move_stepper(stepPin2, step_2_time_index, startup_speed, direction_bool2, num_steps2, ena2);
+    }
+  } while(!(is_off_track1) || !(is_off_track2));
+
+  get_back_on_track();
+
+//  for(int i = 0; i < 100; i++) {
+//    Serial.print("MOTOR1 OFFTRACK?: ");
+//    Serial.println(is_off_track1);
+//    Serial.print("MOTOR2 OFFTRACK?: ");
+//    Serial.println(is_off_track2);
+//  }
 }
 
 void perception() {
-  joy_input1 = analogRead(joy_in1);
-  joy_input2 = analogRead(joy_in2);
 
-  off_track1 = digitalRead(gate_switch1);
-  off_track2 = digitalRead(gate_switch2);
+  get_joystick_inputs(); 
+
+  get_is_off_tracks();
 }
 
 void joystick_direction(const int& in, int& dir, int& ena, const int& off_track) {
-  if(off_track == LOW) {
-    if (in < (analog_in_half_range - joy_deadzone)) {
-      ena = LOW;
-      dir = LOW;
-    }
-    else if (in > (analog_in_half_range + joy_deadzone)) {
-      ena = LOW;
-      dir = HIGH;
-    }
-    else {
-      ena = HIGH;
-    } 
+  if (in < (analog_in_half_range - joy_deadzone)) {
+    ena = LOW;
+    dir = LOW;
+  }
+  else if (in > (analog_in_half_range + joy_deadzone)) {
+    ena = LOW;
+    dir = HIGH;
   }
   else {
-    if (in < (analog_in_half_range - joy_deadzone)) {
-      ena = LOW;
-      dir = HIGH;
-    }
-    else if (in > (analog_in_half_range + joy_deadzone)) {
-      ena = LOW;
-      dir = LOW;
-    }
-    else {
-      ena = HIGH;
-    }
-    delayMicroseconds(200); 
+    ena = HIGH;
   }
 }
 
@@ -174,14 +213,14 @@ unsigned int stepper_wait_time(const int& input) {
 }
 
 void planning() {
-  joystick_direction(joy_input1, direction_bool1, ena1, off_track1);
+  joystick_direction(joy_input1, direction_bool1, ena1, is_off_track1);
   step_1_time_tolerance = stepper_wait_time(joy_input1);
   
-  joystick_direction(joy_input2, direction_bool2, ena2, off_track2);
+  joystick_direction(joy_input2, direction_bool2, ena2, is_off_track2);
   step_2_time_tolerance = stepper_wait_time(joy_input2);
 }
 
-void move_stepper(const unsigned int& step_pin, unsigned long& time_index, const unsigned int& time_tolerance, const unsigned int& direction_bool, long& num_steps, const int& ena) {
+void move_stepper(const unsigned int& step_pin, unsigned long& time_index, const unsigned int& time_tolerance, const unsigned int& direction_bool, uint64_t& num_steps, const int& ena) {
   if ((micros() - time_index) > time_tolerance) {
     digitalWrite(step_pin, HIGH);
     digitalWrite(step_pin, LOW);
@@ -203,5 +242,28 @@ void action() {
   set_direction_bools();
 
   move_stepper(stepPin1, step_1_time_index, step_1_time_tolerance, direction_bool1, num_steps1, ena1);
-  move_stepper(stepPin2, step_2_time_index, step_2_time_tolerance, direction_bool2, num_steps2, ena2);
+  move_stepper(stepPin2, step_2_time_index, step_2_time_tolerance, direction_bool2, num_steps2, ena2); 
+
+//  if (is_off_track1 || is_off_track2) {
+//    get_back_on_track();
+//  }
+//  else {
+//    move_stepper(stepPin1, step_1_time_index, step_1_time_tolerance, direction_bool1, num_steps1, ena1);
+//    move_stepper(stepPin2, step_2_time_index, step_2_time_tolerance, direction_bool2, num_steps2, ena2); 
+//  }
+}
+
+void print(uint64_t value){
+  
+    const int NUM_DIGITS    = log10(value) + 1;
+
+    char sz[NUM_DIGITS + 1];
+    
+    sz[NUM_DIGITS] =  0;
+    for ( size_t i = NUM_DIGITS; i--; value /= 10)
+    {
+        sz[i] = '0' + (value % 10);
+    }
+    
+    Serial.println(sz);
 }
